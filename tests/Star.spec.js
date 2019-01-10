4698d2bf348f17a856a1f3e99ee3e10200fa5f36
André Domingues <voxivoid@gmail.com>
2019-01-10 15:17:31 +0000

    feat: tests setup

---
+++ tests/Star.spec.js
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+
+import Star from "@/components/Star.vue";
+
+
+describe("Star", () => {
+  const wrapper = mount(Star);
+
+  it("shows the correct icon when not starred", () => {
+    wrapper.setProps({ starred: false });
+
+    const html = wrapper.element.outerHTML;
+    expect(html).toMatchSnapshot();
+  });
+
+  it("shows the correct icon when starred", () => {
+    wrapper.setProps({ starred: true });
+
+    const html = wrapper.element.outerHTML;
+    expect(html).toMatchSnapshot();
+  });
+});
